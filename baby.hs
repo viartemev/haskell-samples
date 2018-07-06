ac638cc03e0652120e71dc237155065d77fa5a66
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2018-07-06 10:34:00 +0300

    add new methods

--- baby.hs
+++ baby.hs
@@ -157,3 +157,98 @@ zip' :: [a] -> [b] -> [(a,b)]
 zip' _ [] = []
 zip' [] _ = []
 zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+        | a == x = True
+        | otherwise = a `elem'` xs
+
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (10/)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a  
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]  
+zipWith' _ [] _ = []  
+zipWith' _ _ [] = []  
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys  
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = g
+  where
+    g x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+    | f x = x: filter' f xs
+    | otherwise = filter' f xs
+
+largestDivisible :: (Integral a) => a  
+largestDivisible = head(filter p [100000,99999..])
+    where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]  
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n = n : chain (n * 3 + 1)
+
+numLongChains :: Int  
+numLongChains = length (filter isLong (map chain [1..100]))  
+    where isLong xs = length xs > 15  
+
+
+sum1' :: (Num a) => [a] -> a  
+sum1' xs = foldl (\acc x -> acc + x) 0 xs
+
+sum2' :: (Num a) => [a] -> a  
+sum2' = foldl (+) 0  
+
+elem1' :: (Eq a) => a -> [a] -> Bool  
+elem1' y ys = foldl (\acc x -> if x == y then True else acc) False ys  
+
+elem2' :: (Eq a) => a -> [a] -> Bool  
+elem2' y ys = foldr (flip (\acc x -> if x == y then True else acc)) False ys  
+
+map2' :: (a -> b) -> [a] -> [b]  
+map2' f xs = foldr (\x acc -> f x : acc) [] xs  
+
+filter3' :: (a -> Bool) -> [a] -> [a]  
+filter3' f xs = foldr filterLogic [] xs  
+    where filterLogic = (flip(\acc x -> if(f x) then x:acc else acc))
+
+sum3' :: (Num a) => [a] -> a  
+sum3' = foldl1 (+)
+
+maximum2' :: (Ord a) => [a] -> a  
+maximum2' = foldr1 (\x acc -> if x > acc then x else acc)  
+  
+reverse2' :: [a] -> [a]  
+reverse2' = foldl (\acc x -> x : acc) []  
+  
+product2' :: (Num a) => [a] -> a  
+product2' = foldr1 (*)  
+  
+filter2' :: (a -> Bool) -> [a] -> [a]  
+filter2' p = foldr (\x acc -> if p x then x : acc else acc) []  
+  
+head2' :: [a] -> a  
+head2' = foldr1 (\x _ -> x)  
+  
+last2' :: [a] -> a  
+last2' = foldl1 (\_ x -> x)  
