3186bcf2f4d9706b00421f7d216b8bbacc4625a6
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2018-07-16 13:12:38 +0300

    add custom Tree implementation

---
+++ Others/RecursiveDataStructures.hs
@@ -0,0 +1,30 @@
+infixr 5 :-:  
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)  
+
+infixr 5  .++  
+(.++) :: List a -> List a -> List a   
+Empty .++ ys = ys  
+(x :-: xs) .++ ys = x :-: (xs .++ ys)  
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a  
+singleton x = Node x EmptyTree EmptyTree  
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a  
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+            | x == a = Node x left right  
+            | x < a  = Node a (treeInsert x left) right  
+            | x > a  = Node a left (treeInsert x right)  
+
+treeElem :: (Ord a) => a -> Tree a -> Bool  
+treeElem x EmptyTree = False  
+treeElem x (Node a left right)  
+    | x == a = True  
+    | x < a  = treeElem x left  
+    | x > a  = treeElem x right  
+
+reverseTree :: (Ord a) => Tree a -> Tree a
+reverseTree EmptyTree = EmptyTree
+reverseTree (Node a left right) = Node a right left
